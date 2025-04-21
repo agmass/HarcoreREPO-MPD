843d33785bcfae8b5591e756bf3400867f96d210
agmas <Maxlion360@gmail.com>
2025-04-21 23:17:40 +0400

    Initial Commit

---
+++ HardcoreMode.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using HarmonyLib;
+using Photon.Pun;
+using UnityEngine;
+
+namespace HardcoreREPO;
+
+[HarmonyPatch(typeof(PlayerAvatar))]
+static class Worthless
+{
+    public static ArrayList permaDeadIndividuals = new ArrayList();
+    [HarmonyPrefix, HarmonyPatch(nameof(PlayerAvatar.PlayerDeathDone))]
+    private static void Death_Prefix(PlayerAvatar __instance)
+    {
+        if (!__instance.isLocal) {
+            permaDeadIndividuals.Add(__instance.steamID);
+        }
+    }
+
+
+    [HarmonyPrefix, HarmonyPatch(nameof(PlayerAvatar.Update))]
+    private static void Respawn_Prefix(PlayerAvatar __instance)
+    {
+        if (!__instance.isLocal) {
+            if (permaDeadIndividuals.Contains(__instance.steamID) && !__instance.deadSet) {
+                __instance.PlayerDeath(-1);
+            }
+        }
+    }
+
+
+			
+}
---
+++ MadelinesPermaDeath/README.md
@@ -0,0 +1,17 @@
+Makes every dead player stay dead. *Does not affect the host player*.
+
+**This is reccomended to be played with randoms (and a kick menu)!** Your friends probbably won't like you with this mod (unless you have a; lot; of friends.)
+
+## Can I-
+
+No. There is no way to revive people.
+Dead people will retain throughout the game being open. To reset this, close and restart your game.
+Dead people will remain dead through the shop, setup and every round. This system is also immune to cheaters as if they attempt to revive themselves, it will also count, and instantly kill them.
+
+And now; for people who wanted to be in the mod:
+
+
+### Why "Madeline's Perma-death"? 
+
+Because of my steam name, and a guy who REALLY wanted me to name the mod after it.
+... :3
